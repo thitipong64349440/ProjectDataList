6c3399197e9f2af2be1b0ea7b6f758b5c197007b
thitipong64349440 <141795216+thitipong64349440@users.noreply.github.com>
2023-11-04 17:56:07 +0700

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -11,20 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use('/assets', express.static('public'))
 
-// require('./userPassport')
-
 require('./routes')(app)
 
-// app.get('/status', function (req, res) {
-//     res.send('Hello nodejs server')
-// })
-// app.get('/hello/:person', function (req, res) {
-//     console.log('hello - ' + req.params.person)
-//     res.send('sey hello with ' + req.params.person)
-// })
-// app.post('/hello', function (req, res) {
-//     res.send('OK you post - ' + req.body.name)
-// })
 let port = process.env.PORT || config.port
 
 sequelize.sync({ force: false }).then(() => {
